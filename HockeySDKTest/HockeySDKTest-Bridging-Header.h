71b37dedb7d6904c4543a01022ebceee79fa325e
Bruno Scheele <bruno@noodlewerk.com>
2015-02-09 15:16:35 +0100

    Add bridging header for HockeySDK.

---
+++ HockeySDKTest/HockeySDKTest-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  HockeySDK-Bridging-Header.h
+//  HockeySDKTest
+//
+//  Created by Bruno Scheele on 09/02/15.
+//  Copyright (c) 2015 Binary Adventures. All rights reserved.
+//
+
+#ifndef HockeySDKTest_HockeySDK_Bridging_Header_h
+#define HockeySDKTest_HockeySDK_Bridging_Header_h
+
+#import <Foundation/Foundation.h>
+#import <HockeySDK/HockeySDK.h>
+
+#endif
