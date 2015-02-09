c9afb4ae493fd11e188ce689780c8db30a5491ad
Bruno Scheele <bruno@noodlewerk.com>
2015-02-09 15:01:14 +0100

    Initial Commit

---
+++ HockeySDKTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HockeySDKTest
+//
+//  Created by Bruno Scheele on 09/02/15.
+//  Copyright (c) 2015 Binary Adventures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
